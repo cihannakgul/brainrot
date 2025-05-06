995eab947a11751446b29b5a415e311d565453ab
cihan <cihannakgul@gmail.com>
2025-05-07 01:08:07 +0300

    Initial Commit

---
+++ brainrot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  brainrot
+//
+//  Created by Cihan Akgül on 7.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ brainrot/brainrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainrotApp.swift
+//  brainrot
+//
+//  Created by Cihan Akgül on 7.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct brainrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
